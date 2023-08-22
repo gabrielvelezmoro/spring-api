20c51ff647bec2347e3b15dc004e7ac7447f5801
gabri <gabrieuvelezmoro@gmail.com>
2023-08-22 18:06:18 -0300

    feat: find cliente that id is equal 1

--- src/main/java/gabrielvelezmoro/VendasApplication.java
+++ src/main/java/gabrielvelezmoro/VendasApplication.java
@@ -1,8 +1,6 @@
 package gabrielvelezmoro;
 import gabrielvelezmoro.domain.entity.Cliente;
-import gabrielvelezmoro.domain.entity.Pedido;
 import gabrielvelezmoro.domain.repository.Clientes;
-import gabrielvelezmoro.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,40 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
-import static java.lang.System.*;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos
-    ){
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
         return args -> {
-            out.println("Salvando Clientes");
-            Cliente gabriel = new Cliente("Gabriel");
-            clientes.save(gabriel);
-
-            Pedido p = new Pedido();
-
-            p.setCliente(gabriel);
-            p.setDataPedidio(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-
-       pedidos.findByCliente(gabriel).forEach(out::println);
-
-
+            Cliente c = new Cliente(1,"fulano");
+            clientes.save(c);
         };
     }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
